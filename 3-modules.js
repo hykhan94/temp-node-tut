e41ef9da6760e92a368c4e843d0823740047be01
Hannan Yusuf Khan <hykhan94@gmail.com>
2023-02-28 01:17:32 +0500

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//Modules
+
+const names = require('./4-names')
+const sayHi = require('./5-util')
+console.log(names)
+console.log(sayHi)
+const data = require('./6-alternative-flavour');
+require('./7-mind-grenade');
+
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter)
